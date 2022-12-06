2eeea415bca9362e5eeb0d4f810816fbe0dc590a
Kevin Stewart <kevin.g.stewart@gmail.com>
2022-12-06 09:36:13 -0500

    Update urlupdater.sh

--- urlupdater.sh
+++ urlupdater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## F5 Custom URL Category Update Script
 ## Description: provides options to add URLs, delete URLs, and list the URLs in an existing custom URL category
-## Version: 2.0
+## Version: 2.1
 ## Author: Kevin Stewart
 ## Requires: bash, curl, jq
 ##
@@ -10,7 +10,8 @@
 ##  -l     = list entries in the URL category
 ##  -a     = add a single entry to the URL category
 ##  -d     = delete a single entry from the URL category
-##  -f     = used with -a and -d to specify a file to read from
+##  -t     = replace the URL category (overwrite)
+##  -f     = used with -a, -d, and -t to specify a file to read from
 ##  -b     = the IP address of the BIG-IP
 ##  -u     = username for the BIG-IP (will prompt for password)
 ##  
@@ -21,6 +22,7 @@
 ##  Add file entries:     $0 -b 172.16.1.84 -u admin -c test-category -a file -f testfile.txt
 ##  Delete single entry:  $0 -b 172.16.1.84 -u admin -c test-category -d https://www.foo.com/
 ##  Delete file entries:  $0 -b 172.16.1.84 -u admin -c test-category -d file -f testfile.txt
+##  Replace all entries:  $0 -b 172.16.1.84 -u admin -c test-category -t file -f testfile.txt
 ##
 ## URL format: supplied URLs must be in the following format:
 ##  https://URL/
@@ -36,6 +38,7 @@ help() {
    echo " -l     = list entries in the URL category"
    echo " -a     = add a single entry to the URL category"
    echo " -d     = delete a single entry from the URL category"
+   echo " -t     = replace the URL category (overwrite)"
    echo " -f     = used with -a and -d to specify a file to read from"
    echo " -b     = the IP address of the BIG-IP"
    echo " -u     = username for the BIG-IP (will prompt for password)"
@@ -47,6 +50,7 @@ help() {
    echo " Add file entries:     $0 -b 172.16.1.84 -u admin -c test-category -a file -f testfile.txt"
    echo " Delete single entry:  $0 -b 172.16.1.84 -u admin -c test-category -d https://www.foo.com/"
    echo " Delete file entries:  $0 -b 172.16.1.84 -u admin -c test-category -d file -f testfile.txt"
+   echo " Replace all entries:  $0 -b 172.16.1.84 -u admin -c test-category -t file -f testfile.txt"
    echo ""
    exit
 }
@@ -124,7 +128,6 @@ delete() {
         modurls=`printf '%s\n' "${urls//$newurl/}"`
         payload="{\"urls\":[${modurls}]}"
         payload=`echo ${payload} |sed "s/\[,/\[/;s/,\]/\]/;s/,,/,/"`
-        echo $payload
     else
         # ignore delentry and read from FILE
         while IPS="" read -r p || [ -n "$p" ]
@@ -143,6 +146,33 @@ delete() {
     curl -sku ${LOGIN} -H 'Content-Type: application/json' -X PATCH "https://${BIGIP}/mgmt/tm/sys/url-db/url-category/$CATEGORY" -d ${payload} >/dev/null 2>/dev/null
 }
 
+# replace entries from category
+replace() {
+    LOGIN="${USERNAME}:${PASSWORD}"
+    
+    # test for and read from FILE, otherwise read single entry
+    if [ -z "$FILE" ]
+    then
+        echo "Please also specify a file with -f" && help
+    else
+        # read from FILE
+        payload=""
+        while IPS="" read -r p || [ -n "$p" ]
+        do
+            if [[ "$p" =~ .*"*".* ]]
+            then
+                newurl="{\"name\":\"$p\",\"type\":\"glob-match\"}"
+            else
+                newurl="{\"name\":\"$p\",\"type\":\"exact-match\"}"
+            fi
+            payload+="${newurl}"
+        done < $FILE
+        payload="{\"urls\":[${payload}]}"
+        payload=`echo ${payload} |sed "s/\[,/\[/;s/,\]/\]/;s/,,/,/"`
+    fi
+    curl -sku ${LOGIN} -H 'Content-Type: application/json' -X PATCH "https://${BIGIP}/mgmt/tm/sys/url-db/url-category/$CATEGORY" -d ${payload} >/dev/null 2>/dev/null
+}
+
 # parse input arguments
 CATEGORY=
 ADDENTRY=
@@ -152,27 +182,32 @@ FILE=
 BIGIP=
 USERNAME=
 PASSWORD=
-while getopts "hlc:a:f:d:b:u:" opt; do
+while getopts "hlc:a:f:d:t:b:u:" opt; do
     case ${opt} in
         h )
           help
           ;;
         l )
-          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete)]" && help; fi
+          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete) -t (replace)]" && help; fi
           COMMAND=list
           ;;
         c )
           CATEGORY=$OPTARG
           ;;
         a )
-          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete)]" && help; fi
+          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete) -t (replace)]" && help; fi
           ADDENTRY=$OPTARG
           COMMAND=add
           ;;
         d )
-          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete)]" && help; fi
+          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete) -t (replace)]" && help; fi
           DELENTRY=$OPTARG
           COMMAND=del
+          ;;
+        t )
+          if [ ! -z "$COMMAND" ]; then echo "Please select only one command: [-l (list) -a (add) -d (delete) -t (replace)]" && help; fi
+          DELENTRY=$OPTARG
+          COMMAND=rep
           ;;
         f )
           FILE=$OPTARG
@@ -197,7 +232,7 @@ while getopts "hlc:a:f:d:b:u:" opt; do
 done
 
 # validate arguments
-if [ -z "$COMMAND" ]; then echo "ERROR: at least one command must be specified [-l (list) -a (add) -d (delete)]" && help; fi
+if [ -z "$COMMAND" ]; then echo "ERROR: at least one command must be specified [-l (list) -a (add) -d (delete) -t (replace)]" && help; fi
 if [ -z "$CATEGORY" ]; then echo "ERROR: -c CATEGORY must be specified" && help; fi
 if [ -z "$BIGIP" ]; then echo "ERROR: -b BIG-IP URL must be specified" && help; fi
 if [ -z "$USERNAME" ]; then echo "ERROR: -u USERNAME must be specified" && help; fi
@@ -207,4 +242,4 @@ if [ ! -z "$FILE" ] && [ ! -f "$FILE" ]; then echo "ERROR: -f FILE does not exis
 if [ "$COMMAND" == "list" ]; then list; fi
 if [ "$COMMAND" == "add" ]; then add; fi
 if [ "$COMMAND" == "del" ]; then delete; fi
-
+if [ "$COMMAND" == "rep" ]; then replace; fi
